a79e343dca2bcdb434cd06a948df80688127697c
David Rajcher (Codaisseur) <davidrajchercodaisseur@Davids-MBP.codaisseur.local>
2022-04-13 12:23:58 +0200

    test file added

---
+++ test.js
@@ -0,0 +1,17 @@
+// user
+
+const user = {
+  name: "David",
+  age: 35,
+  isAdult: false,
+};
+
+// DRY Principle
+// Don't Repeat Yourself
+
+const { name, age, isAdult } = user;
+
+console.log(name);
+
+// GIT
+// Version control
