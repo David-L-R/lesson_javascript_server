c37c0124361b901e625a9515b3d78c3f05082e5d
David Rajcher <davidrajcher@Davids-MBP-2.codaisseur.local>
2022-05-18 14:32:09 +0200

    update instructions

--- server.js
+++ server.js
@@ -2,32 +2,6 @@ const express = require("express");
 
 const app = express();
 
-// console.log("server running on port", 4000);
-
-// REST
-// request => data
-// humburger => food
-// "user info" => { name: "david" }
-
-// "/" => route
-app.get("/", (requset, response) => {
-  //   console.log("we are here!");
-  response.send("hello world");
-});
-
-app.get("/user", (requset, response) => {
-  //   console.log("we are here!");
-
-  const user = {
-    name: "david",
-    age: 35,
-    kids: "too many",
-  };
-
-  response.send();
-  // response.send(user.name);
-});
-
 // GET all users
 
 // GET a user by id
